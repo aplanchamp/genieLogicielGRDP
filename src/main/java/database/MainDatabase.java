7c5d764957051dfd52e79e5d882aae55d468a0fc
aude <aplanchamp@gmail.com>
2015-11-14 18:18:41 +0100

    database functions

--- src/main/java/database/MainDatabase.java
+++ src/main/java/database/MainDatabase.java
@@ -35,7 +35,7 @@ public class MainDatabase {
 	//		}
 	//	}
 
-	public static void initDatabase() throws SQLException {
+	public static void createTableLaboratoire() throws SQLException {
 		Connection connection = getDBConnection();
 		PreparedStatement createPreparedStatement = null;
 
@@ -58,57 +58,102 @@ public class MainDatabase {
 
 	}
 
-//	public static void addLaboratoire(String name,String password, String email, int phoneNumber){
-//		Connection connection = getDBConnection();
-//		String InsertQuery = "INSERT INTO LABORATOIRE" + "(id, name, mail, phoneNumber, password) values" + "(?,?,?,?,?)";
-//		PreparedStatement insertPreparedStatement = null;
-//		insertPreparedStatement = connection.prepareStatement(InsertQuery);
-//		insertPreparedStatement.setInt(1, 1);
-//		insertPreparedStatement.setString(2, "Jose");
-//		insertPreparedStatement.executeUpdate();
-//		insertPreparedStatement.close();
-//	}
+
+	public static void addLaboratoire(String name,String password, String email, int phoneNumber) throws SQLException{
+		Connection connection = getDBConnection();
+		String InsertQuery = "INSERT INTO LABORATOIRE" + "(id, name, mail, phoneNumber, password) values" + "(?,?,?,?,?)";
+		PreparedStatement insertPreparedStatement = null;
+
+		try {
+			connection.setAutoCommit(false);
+
+			insertPreparedStatement = connection.prepareStatement(InsertQuery);
+			insertPreparedStatement.setInt(1, 1);
+			insertPreparedStatement.setString(2, name);
+			insertPreparedStatement.setString(3, email);
+			insertPreparedStatement.setInt(4, phoneNumber);
+			insertPreparedStatement.setString(5, password);
+			insertPreparedStatement.executeUpdate();
+			insertPreparedStatement.close();
+
+			connection.commit();
+		} catch (SQLException e) {
+			System.out.println("Exception Message " + e.getLocalizedMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			connection.close();
+		}
+
+	}
 	
+	public static void getAllLaboratoire() throws SQLException{
+		Connection connection = getDBConnection();
+		String SelectQuery = "select * from LABORATOIRE";
+		PreparedStatement selectPreparedStatement = null;
+
+		try {
+			connection.setAutoCommit(false);
+
+			selectPreparedStatement = connection.prepareStatement(SelectQuery);
+			ResultSet rs = selectPreparedStatement.executeQuery();
+			System.out.println("H2 Database inserted through PreparedStatement");
+			while (rs.next()) {
+				System.out.println("Id "+rs.getInt("id")+" Name "+rs.getString("name")+" Phone "+rs.getString("phoneNumber")+rs.getString("mail")+rs.getString("password"));
+			}
+			selectPreparedStatement.close();
+
+			connection.commit();
+		} catch (SQLException e) {
+			System.out.println("Exception Message " + e.getLocalizedMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			connection.close();
+		}
+
+	}	
+
 	// H2 SQL Prepared Statement Example
-//	private static void insertWithPreparedStatement() throws SQLException {
-//		Connection connection = getDBConnection();
-//		PreparedStatement createPreparedStatement = null;
-//		PreparedStatement insertPreparedStatement = null;
-//		PreparedStatement selectPreparedStatement = null;
-//
-//		String CreateQuery = "CREATE TABLE LABORATOIRE(id int primary key, name varchar(255) NOT NULL, mail varchar(255) NOT NULL, phoneNumber int(10) NOT NULL, password varchar(255) NOT NULL)";
-//		String InsertQuery = "INSERT INTO LABORATOIRE" + "(id, name, mail, phoneNumber, password) values" + "(?,?,?,?,?)";
-//		String SelectQuery = "select * from LABORATOIRE";
-//		try {
-//			connection.setAutoCommit(false);
-//
-//			createPreparedStatement = connection.prepareStatement(CreateQuery);
-//			createPreparedStatement.executeUpdate();
-//			createPreparedStatement.close();
-//
-//			insertPreparedStatement = connection.prepareStatement(InsertQuery);
-//			insertPreparedStatement.setInt(1, 1);
-//			insertPreparedStatement.setString(2, "Jose");
-//			insertPreparedStatement.executeUpdate();
-//			insertPreparedStatement.close();
-//
-//			//			selectPreparedStatement = connection.prepareStatement(SelectQuery);
-//			//			ResultSet rs = selectPreparedStatement.executeQuery();
-//			//			System.out.println("H2 Database inserted through PreparedStatement");
-//			//			while (rs.next()) {
-//			//				System.out.println("Id "+rs.getInt("id")+" Name "+rs.getString("name"));
-//			//			}
-//			//			selectPreparedStatement.close();
-//
-//			connection.commit();
-//		} catch (SQLException e) {
-//			System.out.println("Exception Message " + e.getLocalizedMessage());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			connection.close();
-//		}
-//	}
+	//	private static void insertWithPreparedStatement() throws SQLException {
+	//		Connection connection = getDBConnection();
+	//		PreparedStatement createPreparedStatement = null;
+	//		PreparedStatement insertPreparedStatement = null;
+	//		PreparedStatement selectPreparedStatement = null;
+	//
+	//		String CreateQuery = "CREATE TABLE LABORATOIRE(id int primary key, name varchar(255) NOT NULL, mail varchar(255) NOT NULL, phoneNumber int(10) NOT NULL, password varchar(255) NOT NULL)";
+	//		String InsertQuery = "INSERT INTO LABORATOIRE" + "(id, name, mail, phoneNumber, password) values" + "(?,?,?,?,?)";
+	//		String SelectQuery = "select * from LABORATOIRE";
+	//		try {
+	//			connection.setAutoCommit(false);
+	//
+	//			createPreparedStatement = connection.prepareStatement(CreateQuery);
+	//			createPreparedStatement.executeUpdate();
+	//			createPreparedStatement.close();
+	//
+	//			insertPreparedStatement = connection.prepareStatement(InsertQuery);
+	//			insertPreparedStatement.setInt(1, 1);
+	//			insertPreparedStatement.setString(2, "Jose");
+	//			insertPreparedStatement.executeUpdate();
+	//			insertPreparedStatement.close();
+	//
+	//			//			selectPreparedStatement = connection.prepareStatement(SelectQuery);
+	//			//			ResultSet rs = selectPreparedStatement.executeQuery();
+	//			//			System.out.println("H2 Database inserted through PreparedStatement");
+	//			//			while (rs.next()) {
+	//			//				System.out.println("Id "+rs.getInt("id")+" Name "+rs.getString("name"));
+	//			//			}
+	//			//			selectPreparedStatement.close();
+	//
+	//			connection.commit();
+	//		} catch (SQLException e) {
+	//			System.out.println("Exception Message " + e.getLocalizedMessage());
+	//		} catch (Exception e) {
+	//			e.printStackTrace();
+	//		} finally {
+	//			connection.close();
+	//		}
+	//	}
 
 
 	private static Connection getDBConnection() {
