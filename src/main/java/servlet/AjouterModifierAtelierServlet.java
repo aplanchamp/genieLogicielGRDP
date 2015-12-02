176dfb3bc8675b96ce401218a4a1fa7a5912a7e6
mgasque <warzag@gmail.com>
2015-12-02 18:08:20 +0100

    modifier / ajouter labos

--- src/main/java/servlet/AjouterModifierAtelierServlet.java
+++ src/main/java/servlet/AjouterModifierAtelierServlet.java
@@ -1,5 +1,8 @@
 package servlet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -7,7 +10,25 @@ import spark.Response;
 public class AjouterModifierAtelierServlet extends AbstractServlet{
 
 	@Override
-	public ModelAndView handle(Request req, Response res) throws Exception {
-		return null;
-	}
+	public ModelAndView handle(Request request, Response response) throws Exception {
+		if(request.requestMethod() == "POST"){
+			Map<String, Object> attributes = new HashMap<>();
+	        attributes.put("header", "tata");
+	        attributes.put("user", "coucou");
+	        
+	        System.out.println(request.queryParams("name"));
+	        System.out.println(request.queryParams("email"));
+	        System.out.println(request.queryParams("phone"));
+	        System.out.println(request.queryParams("password"));
+	        System.out.println(request.queryParams("userConnect"));
+	        System.out.println(request.queryParams("passwordConnect"));
+	        return new ModelAndView(attributes, "ajoutermodifier.ftl");
+		}
+		else{
+			Map<String, Object> attributes = new HashMap<>();
+	        attributes.put("header", "titi");
+	        attributes.put("user", "coucou");        
+	        return new ModelAndView(attributes, "ajoutermodifier.ftl");
+
+		}	}
 }
