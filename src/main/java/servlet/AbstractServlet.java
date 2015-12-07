3c13651549f0d203243a0f95e7fc7f9b1328af68
hhrostan <rostan.heloise@gmail.com>
2015-12-07 15:34:06 +0100

    sessions

--- src/main/java/servlet/AbstractServlet.java
+++ src/main/java/servlet/AbstractServlet.java
@@ -15,19 +15,7 @@ import spark.*;
 public abstract class AbstractServlet implements TemplateViewRoute{
 
 	public ModelAndView handle(Request request, Response res) throws Exception {
-		if(request.requestMethod() == "POST"){
-			if(request.session().isNew())
-			{
-				request.session(true);  
-				request.session().attribute("mail",request.queryParams("userConnect"));   
-				request.session().attribute("password",request.queryParams("passwordConnect"));
-				System.out.println("tatta");
-			}
-			else
-			{
-				request.session().id();
-			}
-		}
+		
 		
 
 		return null;
--- src/main/java/servlet/LaboratoireServlet.java
+++ src/main/java/servlet/LaboratoireServlet.java
@@ -9,18 +9,47 @@ import spark.Response;
 
 public class LaboratoireServlet extends AbstractServlet{
 
+
 	@Override
 	public ModelAndView handle(Request req, Response res) throws Exception {
-		if(req.requestMethod() == "GET"){
-			System.out.println("toto");
+
+		if(req.requestMethod() == "GET")
+		{
+			
 			Map<String, Object> attributes = new HashMap<>();
 			attributes.put("header", "Laboratoire");
-			return new ModelAndView(attributes, "laboratoire.ftl");
-		}
-		else {
+			
+				
+				// on créé une nouvelle session si besoin
+				if(req.session().isNew())
+				{
+					System.out.println("toto");
+					req.session(true);  
+					req.session().attribute("mail",req.queryParams("userConnect"));   
+					req.session().attribute("password",req.queryParams("passwordConnect"));
+					System.out.println("tatta");
+				}
+				// sinon on vérifie s'il l'user est authentifié
+
+
+				if (req.session().attribute("mail").equals(req.queryParams("userConnect")) && req.session().attribute("password").equals(req.queryParams("passwordConnect")) )
+				{
+					System.out.println("tatta");
+					return new ModelAndView(attributes, "laboratoire.ftl");
+				}
+				else {
+					res.redirect("/accueil"); 
+					System.out.println("coucou je redirige ");
+					return new ModelAndView(attributes, "accueil.ftl");
+				}
+			}
+		
+		else 
+		{
 			System.out.println("tata");
 			return null;
 		}
-	}
 	
+	}
+
 }
